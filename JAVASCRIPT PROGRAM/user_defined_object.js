33663f811107cca2d1ccbb639145f594ad81aec4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-08-02 11:34:25 -0400

    es6 type function

--- JAVASCRIPT PROGRAM/user_defined_object.js
+++ JAVASCRIPT PROGRAM/user_defined_object.js
@@ -29,31 +29,66 @@
 
 // ES5 parameter constructor(function) concept
 
-function Employee(id, name, salary) {
-  this.id = id;
-  this.name = "John Doe";
-  this.role = "employee";
-  this.age = "25";
-  this.job = "Software Engineer";
-  this.salary = "100000";
+// function Employee(id, name, salary) {
+//   this.id = id;
+//   this.name = "John Doe";
+//   this.role = "employee";
+//   this.age = "25";
+//   this.job = "Software Engineer";
+//   this.salary = "100000";
 
-  this.dis = function() {
-    document.write("This is employee behavior");
-    document.write("<br/>id is" + this.id);
-    document.write("<br/>name is" + this.name);
-    document.write("<br/>role is" + this.role);
-    document.write("<br/>age is" + this.age);
-    document.write("<br/>job is" + this.job);
-    document.write("<br/>salary is" + this.salary);
+//   this.dis = function() {
+//     document.write("This is employee behavior");
+//     document.write("<br/>id is" + this.id);
+//     document.write("<br/>name is" + this.name);
+//     document.write("<br/>role is" + this.role);
+//     document.write("<br/>age is" + this.age);
+//     document.write("<br/>job is" + this.job);
+//     document.write("<br/>salary is" + this.salary);
+//   }
+// }
+
+// var emp1 = new Employee(); // memory created
+
+// emp1.dis(); // we can call behavior
+// alert(emp1.id); // we can access property
+
+// var emp1 = new Employee(100,"John",10000); // memory created
+// var emp2 = new Employee(100,"MIke",10400); // memory created
+// emp1.dis(); // we can access property
+// emp2.dis(); // we can access property
+
+
+// ES6 style object creation 
+
+// class Employee {
+//   id = 100;
+//   name = "John Doe";
+//   salary = 120000;  // not required this keyword 
+
+//   dis() {   // not required this keyword   
+//     document.write("This is employee behavior");
+//     document.write("<br/>id is" + this.id); // to access we have to use this keyword
+//     document.write("<br/>name is" + this.name);
+//     document.write("<br/>salary is" + this.salary);
+//   }
+// }
+
+// let emp1 - new Employee(); // memory created
+// emp1.dis(); // we can call behavior
+
+// WE will use ES6 in Angular 
+
+
+class Employee {
+  id = 100;
+  name = "Raj"
+  salary = 12000;       // not require this keyword. 
+  dis() {             // not require this keyword 
+    document.write("<br/>id is " + this.id);   // to access we have to use this keyword
+    document.write("<br/>name is " + this.name);
+    document.write("<br/>salary is " + this.salary);
   }
 }
-
-var emp1 = new Employee(); // memory created
-
-emp1.dis(); // we can call behavior
-alert(emp1.id); // we can access property
-
-var emp1 = new Employee(100,"John",10000); // memory created
-var emp2 = new Employee(100,"MIke",10400); // memory created
-emp1.dis(); // we can access property
-emp2.dis(); // we can access property
+let emp1 = new Employee();
+emp1.dis();
