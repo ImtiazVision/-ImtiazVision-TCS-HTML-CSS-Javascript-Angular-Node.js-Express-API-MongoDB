6f4c0f4c306744e95c4d360d3ad76c0f1115fc30
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-08-15 17:31:35 -0400

    update

--- JAVASCRIPT PROGRAM/Class Programs Practice/set_demo.js
+++ JAVASCRIPT PROGRAM/Class Programs Practice/set_demo.js
@@ -8,4 +8,10 @@ ss.add("John");
 
 document.write(ss +"<br>");
 document.write("Set size is: "+ss.size+ "<br>");
-document.write("Search the set and see Imtiaz is available or not and if available, return: "+ss.has("Imtiaz")+"<br>");
+document.write("Search the set and see if Imtiaz is available or not and if available, return: "+ss.has("Imtiaz")+"<br>");
+document.write("<br>Search the set and see if Mike is available or not and if not available, return: "+ss.has("Mike")+"<br>");
+
+document.write("<br>To delete an element of the set, we can use ss.delete method");
+ss.delete("Rahim");
+
+document.write("<br>After deleting the Rahim element, the set looks like this: "+ ss +"<br>");
