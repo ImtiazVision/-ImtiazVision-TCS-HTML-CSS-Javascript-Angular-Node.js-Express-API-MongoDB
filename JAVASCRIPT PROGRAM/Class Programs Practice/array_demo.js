0351bacefff498783b314927d98ab658cd5c30f9
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-08-15 12:57:29 -0400

    update

---
+++ JAVASCRIPT PROGRAM/Class Programs Practice/array_demo.js
@@ -0,0 +1,24 @@
+var num1 = [10,20,30,40,50,60];  // Literal style array declaration 
+
+var num2 = new Array(20,30,40,60,90,100); // Memory Creation
+
+document.write("Display array as a string <br>");
+document.write("<br>");
+// In javascript, we can display array on the browser as a string. In Javascript, array displayed on the browser is considered as a string. Other languages does not support this.
+
+document.write(num1);
+document.write("<br>");
+document.write("<br>");
+document.write(num2);
+
+document.write("<br>");
+document.write("<br>");
+document.write(num1.join("-"));
+document.write("<br><br> Get Element using the index position " + num1[0]);
+document.write("<br><br> Get Element using the index position " + num1[1]);
+document.write("<br><br> Size of array is: " + num1.length);
+document.write("<br><br> Display the array using for loop: ");
+
+for(var i=0;i<num1.length;i++) {
+  document.write("<br><br> Value of i is  "+num1[i] + " and index is " + i);
+}
